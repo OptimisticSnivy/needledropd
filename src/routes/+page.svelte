086ddfa2e20209fa088490d814163a6569d3d99c
Anand Mahamuni <m.anand7203@gmail.com>
2024-06-06 15:32:57 +0530

    Init + Init navbar

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div id="title">needledropd.</div>
+
+<style>
+	:global(body) {
+		background-color: #1f2937;
+		color: paleturquoise;
+		font-family: "Ubuntu";
+		display: flex;
+		justify-content: center;
+	}
+
+	#title {
+		margin: 10px;
+		font-size: 30px;
+	}
+</style>
