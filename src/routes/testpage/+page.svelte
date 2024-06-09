1ff0ceaa7201335667196da30b6b11ae728e75c0
Anand Mahamuni <m.anand7203@gmail.com>
2024-06-09 14:14:14 +0530

    test page added, remove after deployment

---
+++ src/routes/testpage/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Albumcard from "../../lib/albumcard.svelte";
+</script>
+
+<slot />
+<div id="title">testpage.</div>
+<div class="cards">
+	<Albumcard />
+	<Albumcard />
+	<Albumcard />
+	<Albumcard />
+	<Albumcard />
+	<Albumcard />
+	<Albumcard />
+	<Albumcard />
+</div>
+
+<style>
+	#title {
+		margin: 20px;
+		font-size: 50px;
+	}
+
+	.cards {
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
