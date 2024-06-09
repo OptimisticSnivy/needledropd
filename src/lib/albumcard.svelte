0a3b5ad702e0e13a4b143928cab1beade562e246
Anand Mahamuni <m.anand7203@gmail.com>
2024-06-09 14:14:52 +0530

    feat: Albumcard defined, define now w/ custom tags!

--- src/lib/albumcard.svelte
+++ src/lib/albumcard.svelte
@@ -1,18 +1,50 @@
 <script>
-	async function getAlbumInfo(ANAME, ARTIST) {
+	import { onMount } from "svelte";
+
+	let url, desc, imgsrc, aname, artist;
+	onMount(async function getAlbumInfo() {
 		const API_KEY = "bf0006fdbb2fe14addcc6f11a07025eb";
+		const ANAME = "Kid A";
+		const ARTIST = "Radiohead";
 		const response = await fetch(
 			`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${API_KEY}&artist=${ARTIST}&album=${ANAME}&format=json`,
 		);
 		const data = await response.json();
-		return data;
-	}
-
-	const ANAME = "Kid A";
-	const ARTIST = "Radiohead";
-	const data = getAlbumInfo(ANAME, ARTIST);
+		imgsrc = data.album.image[3]["#text"];
+		aname = ANAME;
+		artist = ARTIST;
+	});
 </script>
 
-<p>
-	URL is here: {data}
-</p>
+<div class="block">
+	<img src={imgsrc} alt={aname} />
+	<div class="text">
+		<p id="aname">{aname}</p>
+		<p id="artist">{artist}</p>
+	</div>
+</div>
+
+<style>
+	.block {
+		margin: 30px;
+		/* height: 250px; */
+		width: 200px;
+		border: thin solid;
+		border-radius: 10px;
+		padding: 10px;
+	}
+
+	img {
+		height: 200px;
+		width: 200px;
+	}
+
+	.text{
+		margin: 5px;
+		padding: 5px;
+	}
+
+	#aname{
+		font-weight: bold;
+	}
+</style>
--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -3,6 +3,7 @@
 		<a href="/">Home</a>
 		<a href="/profile">Profile</a>
 		<a href="/about">About</a>
+		<a href="/testpage">*</a>
 	</nav>
 </div>
 
